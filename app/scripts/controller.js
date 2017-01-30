d0fd830c1d65d923548e8e9750b84a8df1a96384
brandonkramer <4xsquid@gmail.com>
2017-01-30 21:05:50 +0100

    Finished App

---
+++ app/scripts/controller.js
@@ -0,0 +1,24 @@
+app.controller("listCtrl", function($scope) {
+  // Initial list items
+  $scope.products =
+  [
+    "Milk",
+    "bread",
+    "Cheese"
+  ];
+  // Add item to list
+  $scope.addItem = function() {
+    $scope.errortext = "";
+    if (!$scope.addMe) {return;}
+    if ($scope.products.indexOf($scope.addMe) == -1) {
+        $scope.products.push($scope.addMe);
+    } else {
+      $scope.errortext = "The item is already in your shopping list.";
+    }
+  }
+  // Remove item from list
+  $scope.removeItem = function(x) {
+    $scope.errortext = "";
+    $scope.products.splice(x, 1);
+  }
+});
